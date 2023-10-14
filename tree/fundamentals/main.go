4aaa2e8f37ad4da75d0ea89318d0ba4977123758
Benjamin Schreiber <benjaminschreiber@Benjamins-MacBook-Pro.local>
2023-10-14 13:11:06 -0700

    fundamentals

---
+++ tree/fundamentals/main.go
@@ -0,0 +1,109 @@
+package main
+
+// Basic tree definition
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/*
+The in order traversal of a tree can be described as a depth first search, printing
+the left, root, and right and then unwinding the recursion to the parent.
+*/
+func inOrderTraversal(root *TreeNode) []int {
+	ret := []int{}
+
+	if root == nil {
+		return ret
+	}
+
+	// Left of the tree
+	ret = append(ret, inOrderTraversal(root.Left)...)
+
+	// Root of the tree
+	ret = append(ret, root.Val)
+
+	// Right of the tree
+	ret = append(ret, inOrderTraversal(root.Right)...)
+
+	return ret
+}
+
+/*
+A pre-order traversal can be described as a depth first search yielding root, left, right
+*/
+func preOrderTraversal(root *TreeNode) []int {
+	ret := []int{}
+
+	if root == nil {
+		return ret
+	}
+
+	// Root of the tree
+	ret = append(ret, root.Val)
+
+	// Left of the tree
+	ret = append(ret, preOrderTraversal(root.Left)...)
+
+	// Right of the tree
+	ret = append(ret, preOrderTraversal(root.Right)...)
+
+	return ret
+}
+
+/*
+A post-order traversal can be described as a depth first search yielding left, right, root
+*/
+func postOrderTraversal(root *TreeNode) []int {
+	ret := []int{}
+
+	if root == nil {
+		return ret
+	}
+
+	// Left of the tree
+	ret = append(ret, postOrderTraversal(root.Left)...)
+
+	// Right of the tree
+	ret = append(ret, postOrderTraversal(root.Right)...)
+
+	// Root of the tree
+	ret = append(ret, root.Val)
+
+	return ret
+}
+
+/*
+A level order traversal can be described as a breadth first search:
+
+for layer in tree:
+
+	for child in layer:
+		print child
+*/
+func levelOrderTraversal(root *TreeNode) []int {
+	ret := []int{}
+	q := []*TreeNode{root}
+	var node *TreeNode = nil
+	for len(q) > 0 {
+		node = q[0]
+		ret = append(ret, node.Val)
+		q = q[1:]
+
+		if node.Left != nil {
+			q = append(q, node.Left)
+		}
+
+		if node.Right != nil {
+			q = append(q, node.Right)
+		}
+	}
+
+	return ret
+
+}
+
+func main() {
+
+}
---
+++ tree/fundamentals/main_test.go
@@ -0,0 +1,56 @@
+package main
+
+import "testing"
+
+func TestInOrderTraversal(t *testing.T) {
+	got := inOrderTraversal(&TreeNode{1, &TreeNode{2, &TreeNode{4, nil, nil}, &TreeNode{5, nil, nil}}, &TreeNode{3, nil, nil}})
+	want := []int{4, 2, 5, 1, 3}
+	if len(got) != len(want) {
+		t.Fatalf("got %d want %d", got, want)
+		return
+	}
+	for i := range want {
+		if got[i] != want[i] {
+			t.Fatalf("got %d want %d", got, want)
+		}
+	}
+}
+
+func TestPreOrderTraversal(t *testing.T) {
+	got := preOrderTraversal(&TreeNode{1, &TreeNode{2, &TreeNode{4, nil, nil}, &TreeNode{5, nil, nil}}, &TreeNode{3, nil, nil}})
+	want := []int{1, 2, 4, 5, 3}
+	if len(got) != len(want) {
+		t.Fatalf("got %d want %d", got, want)
+	}
+	for i := range want {
+		if got[i] != want[i] {
+			t.Fatalf("got %d want %d", got, want)
+		}
+	}
+}
+
+func TestPostOrderTraversal(t *testing.T) {
+	got := postOrderTraversal(&TreeNode{1, &TreeNode{2, &TreeNode{4, nil, nil}, &TreeNode{5, nil, nil}}, &TreeNode{3, nil, nil}})
+	want := []int{4, 5, 2, 3, 1}
+	if len(got) != len(want) {
+		t.Fatalf("got %d want %d", got, want)
+	}
+	for i := range want {
+		if got[i] != want[i] {
+			t.Fatalf("got %d want %d", got, want)
+		}
+	}
+}
+
+func TestLevelOrderTraversal(t *testing.T) {
+	got := levelOrderTraversal(&TreeNode{1, &TreeNode{2, &TreeNode{4, nil, nil}, &TreeNode{5, nil, nil}}, &TreeNode{3, nil, nil}})
+	want := []int{1, 2, 3, 4, 5}
+	if len(got) != len(want) {
+		t.Fatalf("got %d want %d", got, want)
+	}
+	for i := range want {
+		if got[i] != want[i] {
+			t.Fatalf("got %d want %d", got, want)
+		}
+	}
+}
